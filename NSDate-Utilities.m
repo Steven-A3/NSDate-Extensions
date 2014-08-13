54fb11fb3f5d97c5e2c0f669197b9a0d670be8cb
Steven <cs@allaboutapps.net>
2014-08-13 23:25:26 +0900

    Change for Xcode 6 build

--- NSDate-Utilities.m
+++ NSDate-Utilities.m
@@ -102,7 +102,7 @@
 	NSDateComponents *components2 = [CURRENT_CALENDAR components:DATE_COMPONENTS fromDate:aDate];
 	
 	// Must be same week. 12/31 and 1/1 will both be week "1" if they are in the same week
-	if (components1.week != components2.week) return NO;
+	if (components1.week != components2.weekOfYear) return NO;
 	
 	// Must have a time interval under 1 week. Thanks @aclark
 	return (abs([self timeIntervalSinceDate:aDate]) < D_WEEK);
